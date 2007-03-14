b60cf5bb7dfabe173926c81e2f8da4f368e46578
askxuefeng <askxuefeng@28d0ba32-b41c-0410-b96d-49d1a54840f3>
2007-03-14 22:47:38 +0000

    Version 0.1 is deprecated

--- trunk/Bookstore/src/com/crackj2ee/bookstore/annotation/FIXME.java
+++
@@ -1,5 +0,0 @@
-package com.crackj2ee.bookstore.annotation;
-
-public @interface FIXME {
-
-}
--- trunk/Bookstore/src/com/crackj2ee/bookstore/annotation/TODO.java
+++
@@ -1,8 +0,0 @@
-package com.crackj2ee.bookstore.annotation;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.TYPE, ElementType.CONSTRUCTOR, ElementType.METHOD})
-public @interface TODO {
-
-}
