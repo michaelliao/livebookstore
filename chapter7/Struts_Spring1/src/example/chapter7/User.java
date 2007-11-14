5135d243625ea2a456f443bd459b48bb8a645125
askxuefeng <askxuefeng@28d0ba32-b41c-0410-b96d-49d1a54840f3>
2007-11-14 13:29:57 +0000

    Initial import.

---
+++ chapter7/Struts_Spring1/src/example/chapter7/User.java
@@ -0,0 +1,23 @@
+package example.chapter7;
+
+public class User {
+
+    private String username;
+    private String password;
+
+    private String email;
+    private String blog;
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getBlog() { return blog; }
+    public void setBlog(String blog) { this.blog = blog; }
+
+}
