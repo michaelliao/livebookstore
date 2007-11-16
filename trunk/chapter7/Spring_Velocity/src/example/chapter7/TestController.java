a17ab68bdea1e6c7ad8fcb033afadba31ae3c58f
askxuefeng <askxuefeng@28d0ba32-b41c-0410-b96d-49d1a54840f3>
2007-11-16 13:18:10 +0000

    Initial import.

---
+++ trunk/chapter7/Spring_Velocity/src/example/chapter7/TestController.java
@@ -0,0 +1,25 @@
+package example.chapter7;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class TestController implements Controller {
+
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String name = request.getParameter("name");
+        if(name==null)
+            name = "spring";
+        Map model = new HashMap();
+        model.put("name", name);
+        model.put("time", new Date());
+        return new ModelAndView("test", model);
+    }
+
+}
