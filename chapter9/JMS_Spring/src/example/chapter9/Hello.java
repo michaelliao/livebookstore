7cdc36698f5546764b3d1ed4aa35413dc4052f3d
askxuefeng <askxuefeng@28d0ba32-b41c-0410-b96d-49d1a54840f3>
2007-11-17 00:20:07 +0000

    Initial import.

---
+++ chapter9/JMS_Spring/src/example/chapter9/Hello.java
@@ -0,0 +1,36 @@
+package example.chapter9;
+
+import javax.management.AttributeChangeNotification;
+import javax.management.Notification;
+import javax.management.NotificationBroadcasterSupport;
+
+public class Hello extends NotificationBroadcasterSupport implements HelloMBean {
+
+    private long sequenceNumber;
+    private String name = "world";
+
+    public String getName() {
+        return name;
+    }
+
+    public synchronized void setName(String name) {
+        String oldValue = this.name;
+        this.name = name;
+        sendNotification(new AttributeChangeNotification(
+                this,                        // 哪个对象的属性被更新了
+                sequenceNumber++,            // 序列号
+                System.currentTimeMillis(),  // 时间戳
+                "Attribute 'name' changed!", // 描述
+                "name",                      // 属性名称
+                String.class.getName(),      // 属性的类型
+                oldValue,                    // 更新前的属性值
+                name                         // 更新后的属性值
+        ));
+    }
+
+    public void sayHello() {
+        System.out.println("Hello, " + name);
+        sendNotification(new Notification("void", this, sequenceNumber++));
+    }
+
+}
